04921d18c52677c9b5726c4bdb3943a06abea607
JG HAN <gojkhan13@gmail.com>
2020-12-12 23:55:14 +0900

    12.12  Path Variable 사용

--- src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.restfulwebservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,10 +16,17 @@ public class HelloWorldController {
 
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
+
         return new HelloWorldBean("Hello World");
     }
     //string값이 아니라 java bean형태로 반환하면 스프링프레임워크에서는
     //text나 object형이 아닌 json 형태로 변환해서 반환한다
 
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+
+    }
 
 }
